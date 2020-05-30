6ce39204a289461143b49bd29e298d3479e0c420
mintaka33 <mintaka33@outlook.com>
2020-05-30 13:46:20 +0800

    modify page margins

--- math.py
+++ math.py
@@ -2,24 +2,38 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
 import numpy as np
+from datetime import datetime
 
-math_group_rows = 25
+math_group_rows = 25*7
 math_group_cols = 6
 math_group_num = 2
+min1, max1 = 5, 25
+min2, max2 = 5, 25
+
+def gen_outfile_name():
+    n = datetime.now()
+    time_str = 'math_'
+    time_str += str(n.year) + '-' + str(n.month).zfill(2) + '-' + str(n.day).zfill(2)
+    time_str += '_' + str(n.hour).zfill(2) + '-' + str(n.minute).zfill(2) + '-' + str(n.second).zfill(2)
+    time_str += '_' + str(n.microsecond).zfill(6)
+    time_str += '.xlsx'
+    return time_str
 
 class MathTests():
     def __init__(self):
         self.testset_add = []
         self.testset_sub = []
-        for i in range(30):
-            for j in range(30):
+        for i in range(min1, max1):
+            for j in range(min2, max2):
                 test = [str(i), '+', str(j), '=', ' ', ' ']
                 self.testset_add.append(test)
-        for i in range(30):
-            for j in range(30):
+        print('INFO: total', len(self.testset_add), 'tests in ADD set')
+        for i in range(min1, max1):
+            for j in range(min2, max2):
                 if (i - j) > 0:
                     test = [str(i), '-', str(j), '=', ' ', ' ']
                     self.testset_sub.append(test)
+        print('INFO: total', len(self.testset_sub), 'tests in SUB set')
 
     def get_tests_add(self, num):
         tests = []
@@ -37,10 +51,10 @@ class MathTests():
 
 class XmlWriter():
     def __init__(self):
-        self.dest_filename = 'out.xlsx'
+        self.dest_filename = gen_outfile_name()
         self.total_cols = math_group_cols * math_group_num
         self.total_rows = math_group_rows
-        self.font_size = 20
+        self.font_size = 21
         self.column_width = 7
         self.mystyle = NamedStyle(name="mystyle")
         self.mystyle.font = Font(name='Arial', size=self.font_size)
@@ -49,11 +63,15 @@ class XmlWriter():
         self.wb.add_named_style(self.mystyle)
         self.ws = self.wb.active
         self.ws.title = "math"
+        self.ws.page_margins.left = 0.5
+        self.ws.page_margins.right = 0.5
+        self.ws.page_margins.top = 0.75
+        self.ws.page_margins.bottom = 0.75
         self.ws.page_setup.fitToWidth = 1
         self.ws.print_options.horizontalCentered = True
         self.ws.print_options.verticalCentered = True
         self.ws.print_options.gridLines = True
-
+        # set column width
         for col_idx in range(self.total_cols):
             self.ws.column_dimensions[get_column_letter(col_idx+1)].width = self.column_width
 
@@ -81,6 +99,7 @@ def gen_tests_xml():
     # save xml file
     xml.save_to_file()
 
+gen_tests_xml()
 gen_tests_xml()
 
 print('done')
