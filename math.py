ee6315f3a70f038a0c8ef921c9ec7da06e48d455
mintaka33 <mintaka33@outlook.com>
2020-05-30 11:52:46 +0800

    refine code

--- math.py
+++ math.py
@@ -1,59 +1,55 @@
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
-
 from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
+import numpy as np
+
+dest_filename = 'out.xlsx'
+math_group_rows = 25
+math_group_cols = 6
+math_group_num = 2
+total_cols = math_group_cols * math_group_num
+total_rows = math_group_rows
+font_size = 20
+column_width = 7
 
 mystyle = NamedStyle(name="mystyle")
-mystyle.font = Font(name='Arial', size=20)
+mystyle.font = Font(name='Arial', size=font_size)
 mystyle.alignment = Alignment(horizontal="center", vertical="center", shrink_to_fit=True)
-
-test1 = '12 + 13 ='
-math_test1 = []
-for i in range(20):
-    math_test1.append(test1)
-
 
 wb = Workbook()
 wb.add_named_style(mystyle)
 
-dest_filename = 'tmp.xlsx'
-
 ws = wb.active
-ws.title = "math"
 
+ws.title = "math2"
 ws.page_setup.fitToWidth = 1
-
 ws.print_options.horizontalCentered = True
 ws.print_options.verticalCentered = True
 ws.print_options.gridLines = True
 
+for col_idx in range(total_cols):
+    ws.column_dimensions[get_column_letter(col_idx+1)].width = column_width
 
-for col_idx in range(12):
-    ws.column_dimensions[get_column_letter(col_idx+1)].width = 7
+math_testset = []
+def gen_math():
+    for i in range(30):
+        for j in range(30):
+            test = [str(i), '+', str(j), '=', ' ', ' ']
+            math_testset.append(test)
+gen_math()
 
-def test1():
-    col_idx, row_idx = 0, 0
-    for col in ws.iter_cols(min_row=1, max_col=9, max_row=22):
-        row_idx = 0
-        if col_idx % 3 == 0:
-            for cell in col:
-                cell.style = mystyle
-                cell.value = math_test1[row_idx]
-                row_idx += 1
-        col_idx += 1
-
-test2 = ['12', '+', '13', '=', ' ', ' ']
-math_test2 = []
-for i in range(30):
-    math_test2.append(test2)
-def test2(s):
-    for row_idx, row in enumerate(ws.iter_rows(min_col=s, max_row=30, max_col=s+5)):
+def gen_math_group(group_idx):
+    start = group_idx * math_group_cols + 1
+    end = start + math_group_cols - 1
+    test_array = np.random.randint(900, size=(total_rows))
+    for row_idx, row in enumerate(ws.iter_rows(min_col=start, max_row=total_rows, max_col=end)):
+        test = math_testset[test_array[row_idx]]
         for col_idx, cell in enumerate(row):
             cell.style = mystyle
-            cell.value = math_test2[row_idx][col_idx]
+            cell.value = test[col_idx]
 
-test2(1)
-test2(7)
+for i in range(math_group_num):
+    gen_math_group(i)
 
 wb.save(filename = dest_filename)
 
